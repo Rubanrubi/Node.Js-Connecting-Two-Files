544d377ce6d0410010623d646fa06bbe470e2a62
Rubanrubi <keerthi@sparkouttech.com>
2020-01-19 14:35:58 +0530

    Connect two file

---
+++ index.js
@@ -0,0 +1,5 @@
+const add = require('./utils.js')
+
+const sum = add(8, 7)
+
+console.log(sum)
---
+++ utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js')
+
+const add = function (a, b) {
+    return a + b;
+}
+
+//Here export the function return to index.js file
+module.exports = add
